0b618b5c77b22367f611d4e18ca2468673de75cc
Frank Erdtmann <erdtmann.frank@gmail.com>
2023-02-16 11:11:52 +0100

    Klasse Figur und Spiel erstellt

--- Schach/src/de/erdtmann/soft/schach/Figur.java
+++ Schach/src/de/erdtmann/soft/schach/Figur.java
@@ -8,36 +8,78 @@ public class Figur {
 	private String name;
 	private int spieler;
 	
+	private Figur(FigurBuilder builder) {
+		this.position = builder.position;
+		this.farbe = builder.farbe;
+		this.hintergrund = builder.hintergrund;
+		this.name = builder.name;
+		this.spieler = builder.spieler;
+	}
+	
 	public int[][] getPosition() {
 		return position;
 	}
-	public void setPosition(int[][] position) {
-		this.position = position;
-	}
+	
 	public CoColor getFarbe() {
 		return farbe;
 	}
-	public void setFarbe(CoColor farbe) {
-		this.farbe = farbe;
-	}
+	
 	public CoColor getHintergrund() {
 		return hintergrund;
 	}
-	public void setHintergrund(CoColor hintergrund) {
-		this.hintergrund = hintergrund;
-	}
+	
 	public String getName() {
 		return name;
 	}
-	public void setName(String name) {
-		this.name = name;
-	}
+	
 	public int getSpieler() {
 		return spieler;
 	}
-	public void setSpieler(int spieler) {
-		this.spieler = spieler;
-	}
 
-	
+	public static class FigurBuilder {
+		
+		private int[][] position;
+		private CoColor farbe;
+		private CoColor hintergrund;
+		private String name;
+		private int spieler;
+
+		public FigurBuilder(int[][] position, CoColor farbe, CoColor hintergrund, String name, int spieler) {
+			this.position = position;
+			this.farbe = farbe;
+			this.hintergrund = hintergrund;
+			this.name = name;
+			this.spieler = spieler;
+		}
+		
+		public FigurBuilder withPosition(int[][] position) {
+			this.position = position;
+			return this;
+		}
+		
+		public FigurBuilder withFarbe(CoColor farbe) {
+			this.farbe = farbe;
+			return this;
+		}
+		
+		public FigurBuilder withHintergrund(CoColor hintergrund) {
+			this.hintergrund = hintergrund;
+			return this;
+		}
+		
+		public FigurBuilder withName(String name) {
+			this.name = name;
+			return this;
+		}
+		
+		public FigurBuilder withSpieler(int spieler) {
+			this.spieler = spieler;
+			return this;
+		}
+		
+		public Figur build() {
+			Figur figur =  new Figur(this);
+			return figur;
+		}
+	}
 }
--- Schach/src/de/erdtmann/soft/schach/start.java
+++ Schach/src/de/erdtmann/soft/schach/start.java
@@ -1,40 +1,11 @@
 package de.erdtmann.soft.schach;
 
-
+import java.util.List;
 
 public class start {
-
-	/**
-	 * @param args
-	 */
+	
 	public static void main(String[] args) {
-		String[][] feld = {{"   "," A "," B "," C "," D "," E "," F "," G "," H "},
-							{" 8 "," T "," S "," L "," D "," K "," L "," S "," T "," 8 "},
-							{" 7 "," B "," B "," B "," B "," B "," B "," B "," B "," 7 "},
-							{" 6 ","   ","   ","   ","   ","   ","   ","   ","   "," 6 "},
-							{" 5 ","   ","   ","   ","   ","   ","   ","   ","   "," 5 "},
-							{" 4 ","   ","   ","   ","   ","   ","   ","   ","   "," 4 "},
-							{" 3 ","   ","   ","   ","   ","   ","   ","   ","   "," 3 "},
-							{" 2 "," B "," B "," B "," B "," B "," B "," B "," B "," 2 "},
-							{" 1 "," T "," S "," L "," K "," D "," L "," S "," T "," 1 "},
-							{"   "," A "," B "," C "," D "," E "," F "," G "," H ","   "}};
 		
-		for (int i = 0; i < feld.length; i++) {
-			for (int j = 0; j < feld[i].length; j++) {
-				if (i > 0 && j > 0 && i < feld.length-1 && j < feld[i].length-1 ) {
-					if ((i % 2 == 1 && j % 2 == 1) || (i % 2 == 0 && j % 2 == 0)) {
-						System.out.print(CoColor.WHITE_BACKGROUND + feld[i][j] + CoColor.RESET);
-					}else {
-						System.out.print(CoColor.BLACK_BACKGROUND + feld[i][j] + CoColor.RESET);	
-					}
-					
-				}else {
-					System.out.print(feld[i][j]);
-				}
-				
-			}
-			System.out.println();
-		}
-
+		Spiel spiel = new Spiel();
 	}
 }
